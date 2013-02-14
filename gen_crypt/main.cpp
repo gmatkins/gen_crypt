86fc8847ed1677c63d9167841ada0f66d3ea8bee
Gentry Atkinson <gmatkins@gmail.com>
2013-02-13 19:34:34 -0600

    silly message

--- gen_crypt/main.cpp
+++ gen_crypt/main.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 
 int main(int argc, char** argv){
-
+    if (argc < 2){
+       cerr << "No file specified." << endl;
+       return 0;
+    }
+    if (argc > 2){
+       cerr << "File command not recognized." << endl;
+       return 0;
+    }
+    char * fileName = argv[1];
 	return 0;
 }
