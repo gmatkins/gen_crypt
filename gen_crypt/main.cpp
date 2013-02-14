16819da77cbf770ea7490aab48e9cf29b8253604
Gentry Atkinson <gmatkins@gmail.com>
2013-02-13 19:49:38 -0600

    first day

--- gen_crypt/main.cpp
+++ gen_crypt/main.cpp
@@ -1,5 +1,13 @@
+/*******************************
+*Author:Gentry Atkinson
+*Date:02/13/2013
+*Gen Crypt: a simple command line encryption-decryption tool
+*written with Linux in mind.
+*******************************/
+
 #include<iostream>
 #include<fstream>
+#include<cstring>
 
 using namespace std;
 
@@ -13,5 +21,9 @@ int main(int argc, char** argv){
        return 0;
     }
     char * fileName = argv[1];
+    if (strcmp(fileName + strlen(fileName) - 4, ".gnc") == 0)
+       //gen_crypt produces a plain text file from the .gnc file
+    else
+        //gen_crypt produces a .gnc file from the plain text
 	return 0;
 }
